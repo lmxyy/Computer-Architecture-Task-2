b73cd239fa525a6032f83526091e201a31b72003
lmxyy <664789531@qq.com>
2017-12-18 02:45:13 +0800

    add get_instr.sh

---
+++ Tools/get_instr.sh
@@ -0,0 +1,5 @@
+riscv32-unknown-elf-as -o $1.o -march=rv32i $1.s 
+riscv32-unknown-elf-ld $1.o -o $1.om
+riscv32-unknown-elf-objcopy -O binary $1.om $1.bin
+../../Tools/Bin_to_Text $1.bin > instr.data
+rm $1.o $1.om $1.bin
